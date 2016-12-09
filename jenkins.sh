1a07c258e735864bba1f550cf18a38b1f860614d
Kristofer Andersen <kristofer15@ru.is>
2016-12-09 16:26:14 +0000

    added logs to gitignore. attempting to fix the jenkins script

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,15 @@
+#Remove old node_modules
+rm -rf node_modules
+rm -rf ./client/node_modules
+#Remove everything else
 git clean -dfx
 git stash
-exit ./Makefile
+
+#install npm again
+npm install
+cd client
+npm install
+cd ..
+#Build
+./Makefile
+
