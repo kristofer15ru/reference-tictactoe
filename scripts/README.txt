e1ef78b4f43281cc713ba0339280f16216d498b0
Kristofer Andersen <kristofer15@ru.is>
2016-12-14 21:39:04 +0000

    Moved scripts to a new scripts directory

---
+++ scripts/README.txt
@@ -0,0 +1,1 @@
+Only the Makefile can run in this directory. The rest only work when called from somewhere else.
---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+# Work in current location
+export NODE_PATH=.
+# Clean previous build if it does exist
+npm run clean
+# Make the build directory if it does not exist
+npm run createbuild
+# Run this script on the client if it exists
+npm run buildclient
+# Make the client build the new static build
+mv client/build build/static
+# copy the server into the new build
+cp -R server build/server
+# Create directory tree needed later
+mkdir -p build/client/src
+# Copy vital files that are not built
+cp -r client/src/common build/client/src
+cp run.js ./build
+cp package.json ./build
+#Exit with the last return code
+exit $?
