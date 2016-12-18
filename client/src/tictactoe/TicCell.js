dca1003ce34f9a9a2fa0b81a9272e819f6bbcdb6
Kristofer Andersen <kristofer15@ru.is>
2016-12-18 21:55:13 +0000

    Wrote and passed 'should record move with matching game id and coordinates' for ticCell. game ui can now interact with server

--- client/src/tictactoe/TicCell.js
+++ client/src/tictactoe/TicCell.js
@@ -9,15 +9,52 @@ export default function (injected) {
         constructor() {
             super();
             this.state = {
+              gameId: -1,
+              location: -1,
+              symbol: ''
             }
         }
         componentWillMount(){
+          var location = this.props.coordinates.x + this.props.coordinates.y*3;
+          this.setState({ gameId: this.props.gameId})
+          this.setState({ location: location})
+          const movePlaced = (movePlaced)=>{
+                if(movePlaced.location == this.state.location) {
+                  this.setState({ symbol: movePlaced.side});
+                }
+          };
+
+
+          eventRouter.on('MovePlaced', movePlaced);
+
         }
+
         render() {
-            return <div className="ticcell">
-                CELL
+            //console.debug(this)
+            return <div className="ticcell" onClick={() => this.placeMove(this)} >
+                {this.state.symbol}
             </div>
         }
+
+        placeMove(cell){
+            var cmdId = generateUUID();
+            var gameId = cell.props.gameId;
+            var timeStamp = new Date().getTime();
+            var location = cell.props.coordinates.x + cell.props.coordinates.y*3;
+            var side = cell.props.mySide;
+
+
+            commandPort.routeMessage({
+                gameId: gameId,
+                commandId:cmdId,
+                type:"PlaceMove",
+                timeStamp:timeStamp,
+                location: location,
+                side: side
+            });
+        }
+
+
     }
     return TicCell;
 }
---
+++ client/src/tictactoe/TicCell.test.js
@@ -0,0 +1,77 @@
+import ReactTestUtils from 'react-addons-test-utils';
+import TictactoeBoardModule from './TictactoeBoard';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import TicCellComponent from './TicCell';
+
+import MessageRouter from '../common/framework/message-router';
+
+describe("Tic Cell", function () {
+
+var div, component, TicCell, someTime, side, cmd;
+
+var commandRouter = MessageRouter(inject({}));
+var eventRouter = MessageRouter(inject({}));
+var commandsReceived=[];
+
+commandRouter.on("*", function(cmd){
+    commandsReceived.push(cmd);
+} );
+
+beforeEach(function () {
+    commandsReceived.length=0;
+    someTime = new Date().getTime();
+    side = 'X'
+    cmd = {
+      gameId: 2,
+      commandId:someTime,
+      type:"MovePlaced",
+      timeStamp:someTime,
+      location: 4,
+      side: side
+    }
+    TicCell = TicCellComponent(inject({
+        generateUUID:()=>{
+            return "youyouid"
+        },
+        commandPort: commandRouter,
+        eventRouter
+    }));
+
+
+    div = document.createElement('div');
+
+    component = shallow(<TicCell coordinates={{ x:1, y:1}} gameId={cmd.gameId} mySide={side} />, div);
+
+});
+
+it('should render without error', function () {
+});
+
+it('should record move with matching game id and coordinates ',function(){
+  //Route moveplaced command directly to eventRouter
+  eventRouter.routeMessage(cmd);
+  //Expect this cell to have seen the event and remembered the appropriate data
+  expect(component.state('gameId')).toBe(cmd.gameId)
+  expect(component.state('location')).toBe(cmd.location)
+  //console.debug(component.state('symbol'))
+  //commandRouter.routeMessage(cmd);
+
+});
+/*
+it('should ignore move with matching gameId but not coordinates',function(){
+});
+
+it('should ignore move with matching coordinates, but not matching gameId',function(){
+});
+
+it('should issue PlaceMove command with gameId, mySide and coordinates when clicked', ()=>{
+  component.find('div').simulate('click');
+
+  //check whether correct command was dispatched through command router
+});
+*/
+
+});
